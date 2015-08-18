1e4d66fe72e40b995cae11b6004f40226dfe1802
Andriy Kogut <kogut.andriy@gmail.com>
2015-08-18 16:59:22 +0300

    add auth for build

--- builddiff/jenkins.py
+++ builddiff/jenkins.py
@@ -7,15 +7,17 @@ class Build(object):
 
     """Jenkins build."""
 
-    def __init__(self, url, job, number, data):
+    def __init__(self, auth, url, job, number, data):
         """Wraps jenkins build info.
 
+        :auth: Requests auth instance.
         :url: Jenkins url.
         :job: Job name.
         :number: Build number.
         :data: Info dict.
 
         """
+        self._auth = auth
         self.url = url
         self.job = job
         self.number = number
@@ -27,7 +29,9 @@ class Build(object):
         :returns: string with full console output.
 
         """
-        return requests.get(urljoin(self.url, "/job/{}/{}/consoleText".format(self.job, self.number))).text
+        return requests.get(
+            urljoin(self.url, "/job/{}/{}/consoleText".format(self.job, self.number)), auth=self._auth
+        ).text
 
     def __getitem__(self, attr):
         return self._data[attr]
@@ -64,7 +68,7 @@ class Jenkins(object):
         url = urljoin(self._url, '/job/{}/api/json/build'.format(job))
         params = "depth=1&tree=builds[{}]".format(','.join(tree))
         for build in requests.get(url, params, auth=self._auth).json()['builds']:
-            yield Build(self._url, job, build['number'], build)
+            yield Build(self._auth, self._url, job, build['number'], build)
 
     def matching_builds(self, job, tree, match_map):
         """Find bulds where key matches regex.
